71c534ed002f99eb1791aa684ddb45747752aa1f
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-05-12 17:14:48 +0300

    +build 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAHC0UzaX-UK-irWv04QemcaUTujj2_v3c',
+    authDomain: 'todolist-89791.firebaseapp.com',
+    databaseURL: 'https://todolist-89791.firebaseio.com',
+    projectId: 'todolist-89791',
+    storageBucket: 'todolist-89791.appspot.com',
+    messagingSenderId: '756903341177',
+    appId: '1:756903341177:web:f108e9d3778c7bce29b8a8'
+  }
 };
