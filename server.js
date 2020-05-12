28b411791d0f54a678888867214b9cdaf9f9a53e
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-05-12 17:04:21 +0300

    +build

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/roxy-notes'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/roxy-notes/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/todo-appgit stTG/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
