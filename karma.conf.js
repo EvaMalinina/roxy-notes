04f093beab669825f25bed919a5cf4d877ccf35c
Tatyana Dotsenko <userdotsenko@gmail.com>
2020-05-13 16:51:32 +0300

    +build 17 + ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     // logLevel: config.LOG_DEBUG,
 
     // browsers: ['Chrome', 'ChromeCanary'],
